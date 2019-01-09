a20cdaa0edf9f6b712f9d378be77231f5ded94a2
Tien Pham <r3dsky02@yahoo.com.vn>
2019-01-10 00:10:52 +0200

    Prob

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const Home = () => <div>I'm the home component</div>;
+const Home = () => (
+  <div>
+    <div>I'm the VERY VERY BEST home component</div>
+    <button onClick={() => console.log("Hi there!")}>Press me!</button>
+  </div>
+);
 
 export default Home;
