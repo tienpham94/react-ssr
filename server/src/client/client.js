abebb20ceb84193d94c2e3aeb50bff136b44995f
Tien Pham <r3dsky02@yahoo.com.vn>
2019-01-13 21:58:27 +0200

    Hydrate

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,3 +1,7 @@
-// Startup point for the client side application
 
-console.log('Hi there!');
+// Startup point for the client side application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
