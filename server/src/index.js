8deeb179ac975aaceaa24953f158d36768b4ad21
Tien Pham <r3dsky02@yahoo.com.vn>
2019-01-14 22:54:08 +0200

    Routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,4 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
 import renderer from './helpers/renderer';
 
 const app = express();
